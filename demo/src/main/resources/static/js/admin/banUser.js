3b1a3fe0f9837d419f571cf8856f93fa095e4785
MagicDake <g994538071@gmail.com>
2024-08-18 18:06:15 +0800

    修改banUser页面

--- demo/src/main/resources/static/js/admin/banUser.js
+++ demo/src/main/resources/static/js/admin/banUser.js
@@ -1,77 +1,115 @@
-const API_BASE_URL = "8.130.130.240:8088";
 let currentPage = 1;
 const pageSize = 5;
-let filters = {};
 
 function searchUsers(page, size) {
-    const keyword = document.getElementById('keyword').value;
+    const keyword = document.getElementById('keyword').value.toLowerCase();
+    const statusFilter = document.getElementById('status-filter').value;
+    const sortFilter = document.getElementById('sort-filter').value;
 
-    if (keyword) {
-        console.log(`Searching for keyword: ${keyword}, page: ${page}, size: ${size}`);
+    fetch(`/searchUsers?keyword=${encodeURIComponent(keyword)}&page=${page}&size=${size}`)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(data => {
+            console.log('Received data:', data);
 
-        fetch(`http://8.130.130.240:8088/searchUsers?keyword=${encodeURIComponent(keyword)}&page=${page}&size=${size}`)
-            .then(response => {
-                console.log('Received response:', response);
-                if (!response.ok) {
-                    throw new Error('Network response was not ok');
-                }
-                return response.json();
-            })
-            .then(data => {
-                console.log('Received data:', data);
-                const itemList = document.getElementById('item-list');
-                itemList.innerHTML = ''; // Clear previous results
+            return fetch('/borrow/count')
+                .then(response => response.json())
+                .then(borrowCounts => {
+                    const itemList = document.getElementById('item-list');
+                    itemList.innerHTML = '';
 
-                if (data.content.length > 0) {
-                    data.content.forEach(user => {
-                        const item = document.createElement('div');
-                        item.className = 'item';
+                    let filteredUsers = data.content;
 
-                        const unbanTime = new Date(user.unbanTime);
-                        const localTime = unbanTime.toLocaleString('zh-CN', { timeZone: 'Asia/Shanghai' });
+                    if (statusFilter) {
+                        filteredUsers = filteredUsers.filter(user => user.status === statusFilter);
+                    }
 
-                        item.innerHTML = `
-                        <div class="item-details">
-                            <div class="item-content">
-                                <h3><strong>Username:</strong> ${user.username}</h3>
-                                <p><strong>Password:</strong> ${user.password}</p>
-                                <p><strong>User ID:</strong> ${user.id}</p>
-                                <p><strong>Unban Time:</strong> ${localTime}</p>
-                                <div class="item-meta">
-                                    <select class="ban-options" id="ban-options-${user.id}">
-                                        <option value="7">7 Days</option>
-                                        <option value="30">1 Month</option>
-                                        <option value="182">6 Months</option>
-                                        <option value="3650">Permanent</option>
-                                    </select>
-                                    <button onclick="banUser('${user.id}')">Ban User</button>
-                                </div>
-                            </div>
-                        </div>
-                    `;
-                        itemList.appendChild(item);
+                    if (keyword) {
+                        filteredUsers = filteredUsers.filter(user => user.username.toLowerCase().includes(keyword));
+                    }
+
+                    // 将今年的借阅次数添加到每个用户
+                    filteredUsers.forEach(user => {
+                        user.thisYearBorrowings = borrowCounts[user.id] || 0;
                     });
-                    document.getElementById('no-files').style.display = 'none';
-                    createPagination(data.totalItems, page, size);
-                } else {
-                    document.getElementById('no-files').style.display = 'block';
-                }
-            })
-            .catch(error => {
-                console.error('Error fetching users:', error);
-            });
-    } else {
-        alert('Please enter a search query.');
-    }
+
+                    // 根据选择的排序字段进行排序
+                    if (sortFilter === 'totalBorrowings') {
+                        filteredUsers.sort((a, b) => b.totalBorrowings - a.totalBorrowings);
+                    } else if (sortFilter === 'thisYearBorrowings') {
+                        filteredUsers.sort((a, b) => b.thisYearBorrowings - a.thisYearBorrowings);
+                    }
+
+                    if (filteredUsers.length > 0) {
+                        filteredUsers.forEach(user => {
+                            const item = document.createElement('div');
+                            item.className = 'item';
+
+                            const unbanTime = user.unbanTime ? new Date(user.unbanTime) : null;
+                            const banTime = user.banTime ? new Date(user.banTime) : null;
+                            const localUnbanTime = unbanTime ? unbanTime.toLocaleString('zh-CN', { timeZone: 'Asia/Shanghai' }) : 'N/A';
+                            const localBanTime = banTime ? banTime.toLocaleString('zh-CN', { timeZone: 'Asia/Shanghai' }) : 'N/A';
+
+                            const isBanned = user.status === 'Banned';
+                            const isActive = user.status === 'Active';
+
+                            item.innerHTML = `
+                                <div class="item-details">
+                                    <div class="item-content">
+                                        <h3><strong>Username:</strong> ${user.username}</h3>
+                                        <p><strong>User ID:</strong> ${user.id}</p>
+                                        <p><strong>Status:</strong> ${user.status || 'N/A'}</p>
+                                        <p><strong>Ban Time:</strong> ${localBanTime}</p>
+                                        <p><strong>Unban Time:</strong> ${localUnbanTime}</p>
+                                        <p><strong>Total Borrowings:</strong> ${user.totalBorrowings}</p>
+                                        <p><strong>This Year's Borrowings:</strong> ${user.thisYearBorrowings}</p>
+                                        <div class="item-meta">
+                                            <select class="ban-options" id="ban-options-${user.id}" ${isBanned ? 'disabled' : ''}>
+                                                <option value="7">7 Days</option>
+                                                <option value="30">1 Month</option>
+                                                <option value="182">6 Months</option>
+                                                <option value="3650">Permanent</option>
+                                            </select>
+                                            <button 
+                                                class="${isBanned ? 'disabled-button' : ''}" 
+                                                ${isBanned ? 'disabled' : ''}
+                                                onclick="banUser('${user.id}')">
+                                                Ban User
+                                            </button>
+                                            <button 
+                                                class="${isActive ? 'disabled-button' : ''}" 
+                                                ${isActive ? 'disabled' : ''}
+                                                onclick="unbanUser('${user.id}')">
+                                                Unban User
+                                            </button>
+                                        </div>
+                                    </div>
+                                </div>
+                            `;
+                            itemList.appendChild(item);
+                        });
+                        document.getElementById('no-files').style.display = 'none';
+                        createPagination(filteredUsers.length, page, size);
+                    } else {
+                        document.getElementById('no-files').style.display = 'block';
+                    }
+                });
+        })
+        .catch(error => {
+            console.error('Error fetching users:', error);
+        });
 }
+
 
 function banUser(id) {
     const selectElement = document.getElementById(`ban-options-${id}`);
     const banDuration = selectElement.value;
 
-    console.log(`Ban user with ID: ${id} for ${banDuration}`);
-
-    fetch(`http://8.130.130.240:8088/banUser`, {
+    fetch(`/banUser`, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
@@ -88,15 +126,38 @@ function banUser(id) {
             return response.json();
         })
         .then(data => {
-            console.log('User banned successfully:', data);
             alert('User banned successfully');
-            searchUsers(currentPage, pageSize); // Refresh the user list
+            searchUsers(currentPage, pageSize); // 刷新用户列表
         })
         .catch(error => {
             console.error('Error banning user:', error);
         });
 }
 
+function unbanUser(id) {
+    fetch(`/removeBan`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            userId: id
+        })
+    })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(data => {
+            alert('User unbanned successfully');
+            searchUsers(currentPage, pageSize); // 刷新用户列表
+        })
+        .catch(error => {
+            console.error('Error unbanning user:', error);
+        });
+}
 
 function createPagination(totalElements, currentPage, size) {
     const paginationContainer = document.getElementById('pagination');
@@ -142,8 +203,23 @@ function createPagination(totalElements, currentPage, size) {
     paginationContainer.appendChild(nextButton);
 }
 
+
+// 其他函数保持不变
+
 document.addEventListener('DOMContentLoaded', function () {
+    searchUsers(currentPage, pageSize);
+
     document.getElementById('search-button').addEventListener('click', () => {
+        currentPage = 1;
+        searchUsers(currentPage, pageSize);
+    });
+
+    document.getElementById('status-filter').addEventListener('change', () => {
+        currentPage = 1;
+        searchUsers(currentPage, pageSize);
+    });
+
+    document.getElementById('sort-filter').addEventListener('change', () => {
         currentPage = 1;
         searchUsers(currentPage, pageSize);
     });
